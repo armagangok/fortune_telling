b551d325f9374677bdf7d99c6a32e22bc87732f1
Armağan Gök <1armagangok@gmail.com>
2022-07-26 21:17:27 +0300

    fix: keys

--- lib/feature/models/career_fortune_model.dart
+++ lib/feature/models/career_fortune_model.dart
@@ -2,21 +2,25 @@ import 'package:fortune_telling/core/network/contracts/base_network_model.dart';
 
 class CareerFortuneModel extends BaseNetworkModel {
   String? burc;
+
   String? ozellik;
+
   String? baslik;
+
   String? yorum;
+
   String? unluler;
 
   CareerFortuneModel(
       {this.burc, this.ozellik, this.baslik, this.yorum, this.unluler});
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['Burc'] = this.burc;
-    data['Ozellik'] = this.ozellik;
-    data['Baslik'] = this.baslik;
-    data['Yorum'] = this.yorum;
-    data['Unluler'] = this.unluler;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['Burc'] = burc;
+    data['Ozellik'] = ozellik;
+    data['Baslik'] = baslik;
+    data['Yorum'] = yorum;
+    data['Unluler'] = unluler;
     return data;
   }
 
