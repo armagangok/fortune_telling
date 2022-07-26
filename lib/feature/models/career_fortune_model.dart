e0dfc10148b1fe0c01f09b07d54a2b25b8648d96
Eyüp Ensar <almaz.eyupensar@gmail.com>
2022-07-26 21:12:36 +0300

    My First Commit

--- lib/feature/models/career_fortune_model.dart
+++ lib/feature/models/career_fortune_model.dart
@@ -1,10 +1,41 @@
 import 'package:fortune_telling/core/network/contracts/base_network_model.dart';
 
 class CareerFortuneModel extends BaseNetworkModel {
+  String? burc;
+  String? ozellik;
+  String? baslik;
+  String? yorum;
+  String? unluler;
+
+  CareerFortuneModel(
+      {this.burc, this.ozellik, this.baslik, this.yorum, this.unluler});
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['Burc'] = this.burc;
+    data['Ozellik'] = this.ozellik;
+    data['Baslik'] = this.baslik;
+    data['Yorum'] = this.yorum;
+    data['Unluler'] = this.unluler;
+    return data;
+  }
+
   @override
   fromJson(Map<String, dynamic> json) {
-    // TODO: implement fromJson
-    throw UnimplementedError();
+    return CareerFortuneModel(
+      burc: json['Burc'],
+      ozellik: json['Ozellik'] ?? "",
+      baslik: json['Baslik'],
+      yorum: json['Yorum'],
+      unluler: json['Unluler'],
+    );
   }
-  
+
+  /* CareerFortuneModel.fromJson(Map<String, dynamic> json) {
+    burc = json['Burc'];
+    ozellik = json['Ozellik'];
+    baslik = json['Baslik'];
+    yorum = json['Yorum'];
+    unluler = json['Unluler'];
+  } */
 }
--- lib/feature/models/love_fortune_model.dart
+++ lib/feature/models/love_fortune_model.dart
@@ -1,9 +1,42 @@
 import 'package:fortune_telling/core/network/contracts/base_network_model.dart';
 
 class LoveFortuneModel extends BaseNetworkModel {
+  String? burc;
+  String? ozellik;
+  String? baslik;
+  String? yorum;
+  String? unluler;
+
+  LoveFortuneModel(
+      {this.burc, this.ozellik, this.baslik, this.yorum, this.unluler});
+
   @override
   fromJson(Map<String, dynamic> json) {
-    // TODO: implement fromJson
-    throw UnimplementedError();
+    return LoveFortuneModel(
+      burc: json['Burc'],
+      ozellik: json['Ozellik'] ?? "",
+      baslik: json['Baslik'],
+      yorum: json['Yorum'],
+      unluler: json['Unluler'],
+    );
   }
+
+  /* LoveFortuneModel.fromJson(Map<String, dynamic> json) {
+    burc = json['Burc'];
+    ozellik = json['Ozellik'];
+    baslik = json['Baslik'];
+    yorum = json['Yorum'];
+    unluler = json['Unluler'];
+  } */
+
+  /*
+    Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['Burc'] = this.burc;
+    data['Ozellik'] = this.ozellik;
+    data['Baslik'] = this.baslik;
+    data['Yorum'] = this.yorum;
+    data['Unluler'] = this.unluler;
+    return data;
+  }*/
 }
