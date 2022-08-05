52810ebcbf54859bc22897dc5f6206b91204f8b9
Armağan Gök <1armagangok@gmail.com>
2022-08-05 16:59:29 +0300

    .

---
+++ lib/feature/components/tab_bar_widget.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fortune_telling/core/extension/context_extension.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/instance_manager.dart';
+
+import '../controllers/tab_controller.dart';
+
+class MyTabBar extends StatelessWidget {
+  MyTabBar({Key? key}) : super(key: key);
+
+  final TabBarController tabBarController = Get.put(TabBarController());
+
+  @override
+  Widget build(BuildContext context) {
+    return expandedItems();
+  }
+
+  Widget expandedItems() {
+    return Builder(builder: (context) {
+      return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        physics: const ClampingScrollPhysics(),
+        child: SizedBox(
+          
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              expandedItem(
+                "Genel",
+                0,
+                CupertinoIcons.person_fill,
+                () => tabBarController.changeIndex(0),
+              ),
+              width,
+              expandedItem(
+                "Aşk",
+                1,
+                CupertinoIcons.heart_fill,
+                () => tabBarController.changeIndex(1),
+              ),
+              width,
+              expandedItem(
+                "Sağlık",
+                2,
+                CupertinoIcons.scissors,
+                () => tabBarController.changeIndex(2),
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget get width => Builder(
+        builder: (context) {
+          return SizedBox(
+            width: context.width(0.05),
+          );
+        },
+      );
+
+  Widget expandedItem(
+    String text,
+    int clickedNumber,
+    IconData iconData,
+    final Function onTap,
+  ) {
+    return Builder(
+      builder: (context) {
+        return InkWell(
+          onTap: () => onTap(),
+          child: Obx(
+            () {
+              return Column(
+                children: [
+                  Text(
+                    text,
+                    textAlign: TextAlign.center,
+                    maxLines: 1,
+                    style: TextStyle(
+                      color:
+                          clickedNumber == tabBarController.currentIndex.value
+                              ? Colors.white
+                              : null,
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 134, 20, 255),
+                      ),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(200)),
+                      color:
+                          clickedNumber == tabBarController.currentIndex.value
+                              ? const Color.fromARGB(255, 134, 20, 255)
+                              : Colors.transparent,
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: context.width(0.02),
+                      vertical: context.width(0.02),
+                    ),
+                    child: Icon(
+                      iconData,
+                      size: 30,
+                    ),
+                  ),
+                  
+                ],
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/view/personal/personal_view.dart
+++ lib/view/personal/personal_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fortune_telling/feature/controllers/tab_controller.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/instance_manager.dart';
 
 import '../../core/constants/asset_constant.dart';
 import '../../core/extension/context_extension.dart';
+import '../../feature/components/tab_bar_widget.dart';
 import '../login/controller/zodiac_controller.dart';
 import 'controller/personal_controller.dart';
 
@@ -12,6 +14,8 @@ class PersonalView extends StatelessWidget {
 
   final PersonalController _personalController =
       Get.put(PersonalController.instance);
+
+  final TabBarController _tabBarController = Get.put(TabBarController());
 
   @override
   Widget build(BuildContext context) {
@@ -22,22 +26,46 @@ class PersonalView extends StatelessWidget {
           child: Obx(
             () => _personalController.dailyFortune.value != null
                 ? ListView(
+                    padding: context.symmetric(
+                      horizontal: 0.05,
+                      vertical: 0.025,
+                    ),
                     children: [
-                      zoidacImage(),
-                      zodiacSignText(),
-                      useNametext(),
-                      fortuneText,
+                      zoidacImage,
+                      zodiacSignText,
+                      userNametext,
+                      heigth005,
+                      Center(child: MyTabBar()),
+                      _tabBarController.currentIndex.value == 0
+                          ? fortuneText
+                          : const Center(),
+                      _tabBarController.currentIndex.value == 1
+                          ? const Text("Aşk yorumu yüklenecek")
+                          : const Center(),
+                      _tabBarController.currentIndex.value == 2
+                          ? const Text("SağlıkAşk yorumu yüklenecek")
+                          : const Center(),
                     ],
                   )
                 : const Center(
                     child: Text(
-                        "Burç verileriniz yükleniyor. Lütfen bekleyiniz..."),
+                      "Burç verileriniz yükleniyor. Lütfen bekleyiniz...",
+                    ),
                   ),
           ),
         ),
       ),
     );
   }
+
+  Widget get heigth005 => Builder(
+        builder: (context) {
+          return SizedBox(
+            height: context.height(0.05),
+          );
+        },
+      );
+  //
 
   Decoration get decoration => const BoxDecoration(
         image: DecorationImage(
@@ -46,33 +74,41 @@ class PersonalView extends StatelessWidget {
         ),
       );
 
+  //
+
   Text get fortuneText =>
       Text(_personalController.dailyFortune.value!.fortune!);
 
-  Text useNametext() {
-    return Text(_personalController.userName.value!);
-  }
+  //
 
-  Widget zodiacSignText() {
-    return Builder(builder: (context) {
-      return Text(
-        _personalController.dailyFortune.value!.burc!,
-        textAlign: TextAlign.center,
-        style: context.textTheme.headline3,
+  Text get userNametext => Text(
+        "Merhaba ${_personalController.userName.value!} senin için çok özel bir burç yorumu hazırladık!",
       );
-    });
-  }
 
-  Widget zoidacImage() {
-    return Builder(builder: (context) {
-      return SizedBox(
-        height: context.height(0.15),
-        child: Image.asset(
-          ZodiacController.instance.getZodiacSignImagePath(
+  //
+
+  Widget get zodiacSignText => Builder(
+        builder: (context) {
+          return Text(
             _personalController.dailyFortune.value!.burc!,
-          ),
-        ),
+            textAlign: TextAlign.center,
+            style: context.textTheme.headline3,
+          );
+        },
       );
-    });
-  }
+
+  //
+
+  Widget get zoidacImage => Builder(
+        builder: (context) {
+          return SizedBox(
+            height: context.height(0.15),
+            child: Image.asset(
+              ZodiacController.instance.getZodiacSignImagePath(
+                _personalController.dailyFortune.value!.burc!,
+              ),
+            ),
+          );
+        },
+      );
 }
