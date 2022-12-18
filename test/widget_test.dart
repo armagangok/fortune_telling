7ba8c0fb4a47d7b6913243b9239046f0c0caf79f
Armağan Gök <1armagangok@gmail.com>
2022-12-18 16:37:17 +0300

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fortune_telling/main.dart';
+import 'package:fortune_telling/root.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
